76eebde7eabbb4922c22ac968147c859eaca783e
dora <panagiotopoulou_d@hotmail.com>
2022-12-30 17:34:35 +0200

    Add files via upload

---
+++ sketch_dec30a.ino
@@ -0,0 +1,15 @@
+ #define led_pin 8
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(led_pin,HIGH);
+  delay(1500);
+
+  digitalWrite(led_pin,LOW);
+  delay(1500);
+
+}
